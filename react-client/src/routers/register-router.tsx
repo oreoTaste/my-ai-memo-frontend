34e0bdc18edea808303e439fada182f2598d5f55
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2025-04-04 17:10:15 +0900

    Update register-router.tsx
    회원가입시 텔레그램id를 받도록 수정

--- react-client/src/routers/register-router.tsx
+++ react-client/src/routers/register-router.tsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useDarkMode } from '../contexts/DarkModeContext';
 
-
 export const RegisterRouter = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>();
   const navigate = useNavigate();
@@ -12,13 +11,13 @@ export const RegisterRouter = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   const onSubmit = async (data: any) => {
-    const { loginId, name, password } = data;
+    const { loginId, name, password, telegramId } = data;
     const url = `/user/sign-in`; // 회원가입 API 경로
 
     setIsLoading(true);
     try {
       const response = await axios.post(url
-            , { loginId, name, password }
+            , { loginId, name, password, telegramId }
             , { withCredentials: true, headers: { "X-API-Request": "true" } }
         );
 
@@ -44,26 +43,33 @@ export const RegisterRouter = () => {
   };
 
   return (
-    <div className={`flex flex-col items-center justify-center min-h-screen ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-gray-800'}`}>
+    <div className={`flex flex-col items-center justify-center min-h-screen 
+          ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-gray-800'}`}>
       <h1 className="text-4xl font-semibold mb-6">회원가입</h1>
-      <form
-        className={`p-8 rounded-lg shadow-lg max-w-md w-full ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
-        onSubmit={handleSubmit(onSubmit)}
+      <form className={`p-8 rounded-lg shadow-lg max-w-md w-full 
+              ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
+            onSubmit={handleSubmit(onSubmit)}
       >
         <div className="mb-4">
           <input
             {...register('loginId', {
               required: '아이디를 입력해주세요',
               pattern: {
-                value: /^(admin|[a-zA-Z0-9_]{5,20})$/,
+                value: /^([a-zA-Z0-9_]{5,20})$/,
                 message: '아이디는 5~20자의 영문자, 숫자, 밑줄만 가능합니다.',
               },
             })}
             type="text"
             placeholder="아이디"
-            className={`w-full p-3 border rounded-md text-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${isDarkMode ? 'bg-gray-800 text-white border-gray-600' : 'bg-white text-gray-800 border-gray-300'} ${errors.id ? 'border-red-500' : ''}`}
+            className={`w-full p-3 border rounded-md text-lg focus:outline-none 
+              ${isDarkMode 
+                ? 'bg-gray-800 text-white border-gray-600' 
+                : 'bg-white text-gray-800 border-gray-300'}
+              ${errors.loginId 
+                ? 'border-red-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500' 
+                : 'border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500'}`}
           />
-          {errors.id && <span className="text-red-500 text-sm">{getErrorMessage(errors.id as FieldError)}</span>}
+          {errors.loginId && <span className="text-red-500 text-sm">{getErrorMessage(errors.loginId as FieldError)}</span>}
         </div>
 
         <div className="mb-4">
@@ -74,9 +80,34 @@ export const RegisterRouter = () => {
             })}
             type="text"
             placeholder="이름"
-            className={`w-full p-3 border rounded-md text-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${isDarkMode ? 'bg-gray-800 text-white border-gray-600' : 'bg-white text-gray-800 border-gray-300'} ${errors.name ? 'border-red-500' : ''}`}
+            className={`w-full p-3 border rounded-md text-lg focus:outline-none 
+              ${isDarkMode 
+                ? 'bg-gray-800 text-white border-gray-600' 
+                : 'bg-white text-gray-800 border-gray-300'}
+              ${errors.name 
+                ? 'border-red-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500' 
+                : 'border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500'}`}
           />
           {errors.name && <span className="text-red-500 text-sm">{getErrorMessage(errors.name as FieldError)}</span>}
+        </div>
+
+        <div className="mb-4">
+          <input
+            {...register('telegramId', {
+              required: false,
+              pattern: {
+                value: /^[a-zA-Z0-9_]{5,32}$/,
+                message: '텔레그램 ID는 5~32자의 영문자, 숫자, 밑줄만 가능합니다.',
+              },
+            })}
+            type="text"
+            placeholder="텔레그램 ID"
+            className={`w-full p-3 border rounded-md text-lg focus:outline-none focus:ring-2 focus:ring-blue-500
+              ${isDarkMode 
+                ? 'bg-gray-800 text-white border-gray-600'
+                : 'bg-white text-gray-800 border-gray-300'}`}
+          />
+          {errors.telegramId && <span className="text-red-500 text-sm">{getErrorMessage(errors.telegramId as FieldError)}</span>}
         </div>
 
         <div className="mb-6">
@@ -87,7 +118,13 @@ export const RegisterRouter = () => {
             })}
             type="password"
             placeholder="비밀번호"
-            className={`w-full p-3 border rounded-md text-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${isDarkMode ? 'bg-gray-800 text-white border-gray-600' : 'bg-white text-gray-800 border-gray-300'} ${errors.password ? 'border-red-500' : ''}`}
+            className={`w-full p-3 border rounded-md text-lg focus:outline-none 
+              ${isDarkMode 
+                ? 'bg-gray-800 text-white border-gray-600' 
+                : 'bg-white text-gray-800 border-gray-300'}
+              ${errors.password
+                ? 'border-red-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500' 
+                : 'border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500'}`}
           />
           {errors.password && <span className="text-red-500 text-sm">{getErrorMessage(errors.password as FieldError)}</span>}
         </div>
@@ -95,7 +132,8 @@ export const RegisterRouter = () => {
         <button
           type="submit"
           disabled={isLoading}
-          className={`w-full py-3 text-lg font-semibold rounded-md ${isLoading ? 'bg-gray-400' : 'bg-blue-500'} text-white hover:bg-blue-600 focus:outline-none`}
+          className={`w-full py-3 text-lg font-semibold rounded-md text-white hover:bg-blue-600 focus:outline-none
+            ${isLoading ? 'bg-gray-400' : 'bg-blue-500'}`}
         >
           {isLoading ? '회원가입 중...' : '회원가입'}
         </button>
